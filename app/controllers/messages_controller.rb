fef23e9fc295d37dd13d9acd5a867d6f2c281bb2
Quentin Soubeyrol <legaspirateur@gmail.com>
2021-02-23 22:38:30 +0100

    some work on message through action cable

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,19 +1,19 @@
 class MessagesController < ApplicationController
   def create
-    @dare = Dare.find(params[:dare_id])
-    @challenge = Challenge.find(params[:challenge_id])
     @chatroom = Chatroom.find(params[:chatroom_id])
     @message = Message.new(message_params)
     @message.chatroom = @chatroom
     @message.user = current_user
     if @message.save
-      ChatroomChannel.broadcast_to(
-      @chatroom,
-      render_to_string(partial: "message", locals: { message: @message })
-      )     
       redirect_to chatroom_path(@chatroom, anchor: "message-#{@message.id}")
     else
-      render '/challenges/<%= params[:challenge_id] %>/dares/<%= :dare_id %>/messages'
+      render "chatrooms/show"
     end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_23_094100) do
+ActiveRecord::Schema.define(version: 2021_02_23_211547) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,10 +88,10 @@ ActiveRecord::Schema.define(version: 2021_02_23_094100) do
   create_table "messages", force: :cascade do |t|
     t.string "content"
     t.bigint "user_id", null: false
-    t.bigint "challenge_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["challenge_id"], name: "index_messages_on_challenge_id"
+    t.bigint "chatroom_id"
+    t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
@@ -116,6 +116,6 @@ ActiveRecord::Schema.define(version: 2021_02_23_094100) do
   add_foreign_key "dares", "users"
   add_foreign_key "journeys", "dares"
   add_foreign_key "journeys", "users"
-  add_foreign_key "messages", "challenges"
+  add_foreign_key "messages", "chatrooms"
   add_foreign_key "messages", "users"
 end
