fa766349dc11932a1bf22cbc99a34c2ef9baa665
Gabriel Roux <rouxgab81@gmail.com>
2021-02-23 15:38:52 +0100

    journey almost done

--- app/models/journey.rb
+++ app/models/journey.rb
@@ -3,6 +3,6 @@ class Journey < ApplicationRecord
   belongs_to :dare
   validates :title, length: { maximum: 30 }
   validates :content, presence: true
-  validates :challenge_rating, inclusion: { in: %w(0 1 2 3 4 5) }
+  validates :challenge_rating, inclusion: { in: 0..5 }
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,12 @@ Rails.application.routes.draw do
   resources :challenges, only: [:index, :show, :new, :create, :edit, :update] do
     resources :dares, only: [:index, :new, :create, :edit, :update, :show] do
       resources :messages, only: [:index, :new, :create, :edit, :update]
-      resources :journeys, only: [:index, :new, :create, :edit, :update]
+      resources :journeys, only: [:new, :create, :edit, :update]
     end
   end
-
-
+  resources :journeys, only: [:index]
+  namespace :my do
+    resources :journeys, only: [:index, :edit, :update, :destroy]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
