559d767e8396bbd7788ed6ffd1d3b62fe737fca6
Gabriel Roux <rouxgab81@gmail.com>
2021-02-24 15:36:52 +0100

    fix env

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -6,6 +6,10 @@ class ChallengesController < ApplicationController
     3.times do
       @challenge_sample << @challenges.sample
     end
+    @markers = {
+        lat: 48.8648482,
+        lng: 2.3798534
+      }
   end
 
   def show
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,6 +25,7 @@ require("channels")
 // External imports
 import "bootstrap";
 import { initChatroomCable } from '../channels/chatroom_channel.js';
+import { initMapbox } from '../plugins/init_mapbox';
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
@@ -32,4 +33,5 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   initChatroomCable();
+  initMapbox();
 });
