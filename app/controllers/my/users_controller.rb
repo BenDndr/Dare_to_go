230b7369b7320eb85c6db9bfb6c029ea86690d7b
Benjamin DANDRE <dandre.ben@gmail.com>
2021-02-24 17:30:21 +0100

    wip

--- app/controllers/my/users_controller.rb
+++ app/controllers/my/users_controller.rb
@@ -2,6 +2,8 @@ class My::UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @dares = Dare.where(user_id: params[:id])
+    @dares_complete = @dares.where(progress: "validated")
+    @dares_in_progress = @dares.where(progress: "pending")
     @journeys = Journey.where(user_id: params[:id])
   end
 end
