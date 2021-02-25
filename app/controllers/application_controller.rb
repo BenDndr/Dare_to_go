544fe4c4a4537283387515c610fc92788601ed65
Quentin Soubeyrol <legaspirateur@gmail.com>
2021-02-25 15:34:21 +0100

    my dares et xp bar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,9 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :user_level, :lvl, :user_xp
 
-   def configure_permitted_parameters
+  def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :username, :birth_date, :presentation, :address, :photo])
-
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:email, :username, :birth_date, :presentation, :address, :photo])
   end
@@ -29,10 +28,10 @@ class ApplicationController < ActionController::Base
     if user_signed_in? && !current_user.challenges.nil?
       @user_lvl = Level.where("xp_requirement <= ?", user_xp).last
       lvl_plus = @user_lvl.id + 1
-      @lvl = (user_xp - Level.find(@user_lvl.id).xp_requirement).fdiv(Level.find(lvl_plus).xp_requirement - Level.find(@user_lvl.id).xp_requirement) * 100
+      @lvl = (user_xp - Level.find(@user_lvl.id).xp_requirement).fdiv(Level.find(lvl_plus).xp_requirement -
+        Level.find(@user_lvl.id).xp_requirement) * 100
     else
       @lvl = 0
     end
   end
 end
-
