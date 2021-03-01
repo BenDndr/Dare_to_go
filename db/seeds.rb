9fa23e8ff80634336c35dd05958d50c6ecd4c764
Benjamin DANDRE <dandre.ben@gmail.com>
2021-03-01 17:32:43 +0100

    seed fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -119,7 +119,8 @@ CSV.foreach(filepath, csv_options) do |row|
     content: "#{row['content']}",
     difficulty: "#{row['difficulty'].to_i}",
     xp: "#{row['xp'].to_i}",
-    delay: "#{row['delay'].to_i}"
+    delay: "#{row['delay'].to_i}",
+    incription: "#{row['inscription'].to_i}"
   )
   challenge.photo.attach(io: File.open(Rails.root.join("app/assets/images/challenges/Challenge#{n}.jpeg")), filename: "Challenge#{n}", content_type: 'image/jpeg')
   challenge.save!
