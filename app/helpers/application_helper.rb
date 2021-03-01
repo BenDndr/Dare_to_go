df1d279101349850586ab570673ca304732605f0
Quentin Soubeyrol <legaspirateur@gmail.com>
2021-03-01 11:41:17 +0100

    fb auth ok

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,8 @@ module ApplicationHelper
     if element.photo.attached?
       cl_image_tag(element.photo.key,width: 150, height: 150, crop: :thumb, gravity: :face, class: "avatar dropdown-toggle", id: "navbarDropdown", data: { toggle: "dropdown" }, 'aria-haspopup': true, 'aria-expanded': false)
     else
-      image_tag("avatar-placeholder.png", class: "avatar dropdown-toggle", id: "navbarDropdown", data: { toggle: "dropdown" }, 'aria-haspopup': true, 'aria-expanded': false)
+      avatar_url = current_user.facebook_picture_url
+      image_tag avatar_url, class: "avatar dropdown-toggle", id: "navbar-wagon-menu", "data-toggle" => "dropdown"
     end
   end
 end
